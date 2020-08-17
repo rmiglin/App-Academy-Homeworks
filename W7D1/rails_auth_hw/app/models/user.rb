378ed57b9f443c8fe1a4d6665a61c555594cd797
rmiglin <ross@macbook-pro.myfiosgateway.com>
2020-08-17 03:50:28 -0400

    added User Model to Auth hw

--- W7D1/rails_auth_hw/app/models/user.rb
+++ W7D1/rails_auth_hw/app/models/user.rb
@@ -1,7 +1,41 @@
 class User < ApplicationRecord
-    validates :username, presence: true
-    validates :password_digest, presence: true
+    validates :username, presence: true, uniqueness: true
+    validates :password_digest, presence: {message: 'Password can\'t be blank'}
     validates :password, length: {minimum: 6, allow_nil: true}
+    validates :session_token, presence: true, uniqueness: true
+    before_validation :ensure_session_token
 
-    
+    attr_reader :password
+
+    def self.find_by_credentials(username, pwd)
+        user = User.find_by(username: username)
+        if user && BCrypt::Password.new(user.password_digest).is_password?(pwd)
+            return user
+        else
+            return nil
+        end
+    end
+
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64(16)
+    end
+
+    def reset_session_token!
+        self.session_token = User.generate_session_token
+        self.save!
+        return self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= User.generate_session_token
+    end
+
+    def password=(pwd)
+        @password = pwd
+        self.password_digest = BCrypt::Password.create(pwd)
+    end
+
+
+
+
 end
