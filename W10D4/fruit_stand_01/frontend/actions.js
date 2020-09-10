3e85ef46aeb44a77c37478c90237be13ac3ff817
rmiglin <rossmiglin@gmail.com>
2020-09-10 03:23:39 -0400

    added W10D4 hw

---
+++ W10D4/fruit_stand_01/frontend/actions.js
@@ -0,0 +1,15 @@
+export const addOrange = {
+  type: 'ADD_FRUIT',
+  fruit: 'orange'
+};
+
+// same action type different payload
+export const addApple = {
+  type: 'ADD_FRUIT',
+  fruit: 'apple'
+};
+
+export const addPear = {
+  type: 'ADD_FRUIT',
+  fruit: 'pear'
+};
